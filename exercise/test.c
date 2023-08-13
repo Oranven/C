ef09e212aca12e06c5483e9325954926876f658a
Oranven <2561860767@qq.com>
2023-08-13 20:25:31 +0800

    first commit

--- exercise/test.c
+++ exercise/test.c
@@ -2,6 +2,16 @@
 int main()
 {
 	int hour1, minutes1;
+	int hour2, minutes2;
+	
 	scanf_s("%d %d", &hour1,&minutes1);
+	scanf_s("%d %d", &hour2,&minutes2);
+
+	int t1 = hour1 * 60 + minutes1;
+	int t2 = hour2 * 60 + minutes2;
+	int t = t2 - t1;
+
+	printf("现在的时差是%d h %d min。", t / 60, t % 60);
+
 	return 0;
 }
