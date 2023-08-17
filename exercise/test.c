a5a4c677e423ee252095546e2d4e9377d7903914
Oranven <2561860767@qq.com>
2023-08-17 21:12:19 +0800

    first commit

--- exercise/test.c
+++ exercise/test.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int a = 5;
-	int b = 6;
-	int c;
+	int a;
 
-	c = a;
-	a = b;
-	b = c;
+	a = 10;
 
-	printf("a=%d,b=%d", a, b);
+	printf("a++=%d\n", a++);
+	printf("a=%d\n", a);
+	printf("++a=%d\n", ++a);
+	printf("a=%d\n", a);
+	
+
 	return 0;
 }
