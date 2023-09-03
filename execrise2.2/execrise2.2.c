f021446e9745fbf985fde3bc66afd6479c311ae1
Oranven <2561860767@qq.com>
2023-09-03 12:43:23 +0800

    first commit

--- execrise2.2/execrise2.2.c
+++ execrise2.2/execrise2.2.c
@@ -6,8 +6,8 @@ int main()
 	scanf_s("%d %d", &time1, &time2);
 
 	int a = time1 / 100 + time2 / 60;
-	//a=Сʱ
 	int b = (time1 / 100.0 - time1 / 100) * 100 + time2 % 60;
-	printf("%lf", (a + b / 60 + b % 60 / 100.0) * 100);
+	printf("%lf", (a + b / 60 + b % 60 / 100.0) * 100.0);
+
 	return 0;
 }
